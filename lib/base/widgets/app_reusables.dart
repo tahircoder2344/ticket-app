57e5ac966bf0b554bc8ed1fc67a3c2a6c4033c24
Tahir Coder <chatwithme2344@gmail.com>
2024-06-23 17:30:25 +0500

    new files

---
+++ lib/base/widgets/app_reusables.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+
+class AppReusables extends StatelessWidget {
+  const AppReusables(
+      {super.key, required this.bigText, required this.smallText});
+  final String bigText;
+  final String smallText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(bigText,style: AppStyles.headlineStyleThree,),
+        InkWell(
+          onTap: (){
+
+          },
+          child: Text(smallText,style: AppStyles.textStyle.copyWith(
+            color:AppStyles.primaryColor
+          ),),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/base/widgets/ticket_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TicketView extends StatelessWidget {
+  const TicketView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return  SizedBox(
+      width: size.width*0.85,
+      height: 179,
+      child: Container(
+        margin: const EdgeInsets.only(right: 16),
+        color: Colors.red,
+        child:const Text("Hello"),
+      ),
+    );
+  }
+}
