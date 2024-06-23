160695c724419aae36e00f98c484c295656c0400
Tahir Coder <chatwithme2344@gmail.com>
2024-06-23 17:33:30 +0500

    app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,21 @@ import 'package:ticket_app/base/bottom_navbar.dart';
 
 void main() {
   runApp(const MyApp());
+  // var test = TestClass(x: 4, y: 10);
+  //
+  // var newTest = test.copyWith(x: 30, y: 40);
 }
+
+// class TestClass {
+//    int x;
+//    int y;
+//
+//   TestClass({required this.x, required this.y});
+//
+//   TestClass copyWith({int? x, int? y}) {
+//     return TestClass(x:5,y:20);
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+      debugShowCheckedModeBanner: false,
       home: BottomNavbar(),
     );
   }
