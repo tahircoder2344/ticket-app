9497e26061f4551299f4a8cb16c487bda4cf7338
Tahir Coder <chatwithme2344@gmail.com>
2024-06-23 17:32:22 +0500

    screens of my app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,80 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:ticket_app/base/res/media.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/widgets/app_reusables.dart';
+import 'package:ticket_app/base/widgets/ticket_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-  body: ListView(
-    children:  [
-      Container(child: const Text("Hello this is home screen"),)
-    ],
-  ),
-    );
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        children: [
+          const SizedBox(height: 50,),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: AppStyles.headlineStyleTwo,
+                        ), // Add closing parenthesis
+                        const SizedBox(height: 5),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headlineStyleOne,
+                        ),
+                      ],
+                    ),
+                    Container(
+
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage(AppMedia.logo)
+                        ),
+                      ),
+                    ),
+                  ],
+                ), // Add closing parenthesis
+                const SizedBox(height: 25,),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppStyles.searchBarColor,
+                  ),
+                  child: const Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      Text("Search"),],
+                  ),
+                ),
+                const SizedBox(height: 40,),
+                const AppReusables(bigText: 'Upcoming Flights', smallText: 'View All',),
+
+                const SizedBox(height: 40,),
+                const AppReusables(bigText: 'Upcoming Meetings', smallText: 'View All',),
+                const TicketView()
+              ],
+            ),
+          ),
+
+        ],
+      ), // Remove extra comma
+    ); // Add closing parenthesis
   }
 }
